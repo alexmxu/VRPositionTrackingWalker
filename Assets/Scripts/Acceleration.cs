3bf70f8d1d07e75321b9e9bc43462cbdf86460f7
alexmxu <alexmxu@outlook.com>
2015-07-23 14:41:22 -0500

    commit of past few days fuck you idgaf

---
+++ Assets/Scripts/Acceleration.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using System.Collections.Generic;
+
+public class Acceleration : MonoBehaviour {
+
+	private List<Vector3> positions;
+	private List<Vector3> velocities;
+	private List<Vector3> accelerations;
+	public GameObject player; 
+	private int frames = 1;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<GameObject>();
+		positions = new List<Vector3>();
+		velocities = new List<Vector3>();
+		accelerations = new List<Vector3>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(VRDevice.isPresent){
+			VRRecording();
+		}
+		else{
+			PCRecording();
+			frames++;
+
+		}
+		//Debug.Log ("The total number of time passed is " + Time.time);
+	}
+
+	//gets position data from VR Device
+	void VRRecording () {
+		positions.Add(InputTracking.GetLocalPosition(VRNode.CenterEye));
+		CalcAcceleration();
+	}
+
+	//gets position data from PC
+	void PCRecording () {
+		positions.Add (transform.position);
+		//Debug.Log(positions[frames]);
+
+		CalcAcceleration();
+	}
+
+	void CalcAcceleration(){
+		Vector3 posChange = positions[frames] - positions[frames - 1];
+
+		velocities.Add(posChange/Time.deltaTime);
+		Vector3 velChange = velocities[frames] - velocities[frames - 1];
+		//Debug.Log(velocities[frames]);
+
+		accelerations.Add(velChange/Time.deltaTime);
+		Debug.Log(accelerations[frames] + ", " + Time.deltaTime + ", " + frames);
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	private float lookAngle;
+	private float rotationGain = 4.0f;
+	public Rigidbody rb;
+	public float movespeed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+		
+		float mouseXPosition = Input.GetAxis ("Mouse X");
+		lookAngle += mouseXPosition * rotationGain;
+		Quaternion rotation = Quaternion.AngleAxis (lookAngle, new Vector3 (0.0f, 1.0f, 0.0f));
+		transform.rotation = rotation;
+
+//	}
+//	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		transform.Translate (movement * Time.deltaTime * movespeed);
+	}
+}
+//WE ARE IMPLEMENTING VR INTO LOOK AND MOVEMENT
+//with InputTracking.GetLocalRotation
